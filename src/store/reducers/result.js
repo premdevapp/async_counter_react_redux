771bbd9c3d8f0a43935df911a55a9588ae88e669
premnath <premdevapp@gmail.com>
2020-12-24 15:48:59 +0530

    all changes added

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,27 +1,27 @@
-import * as actionTypes from "../actions/actions";
+import * as actionTypes from "../actions/actionsTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) =>
+  state.results.filter((result) => result.id !== action.resultElId);
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result }),
-      };
+      });
+
     case actionTypes.DELETE_RESULT:
       // const id = 2;
       // const newArray = [...state.results];
       // newArray.splice(id, 1)
-      const updatedArray = state.results.filter(
-        (result) => result.id !== action.resultElId
-      );
-      return {
-        ...state,
-        results: updatedArray,
-      };
+
+      return updateObject(state, { results: deleteResult(state, action) });
+
     default:
       return state;
   }
