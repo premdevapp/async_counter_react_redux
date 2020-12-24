ec0136c54dbc3c99cda7b6618245f2bf86f5c46f
premnath <premdevapp@gmail.com>
2020-12-24 13:39:42 +0530

    ch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
 import counterReducer from "./store/reducers/counter";
 import resultReducer from "./store/reducers/result";
@@ -15,7 +15,23 @@ const rootReducer = combineReducers({
   res: resultReducer,
 });
 
-const store = createStore(rootReducer);
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("{MiddleWare} - Dispatching", action);
+      const result = next(action);
+      console.log("{MiddleWare} - next state", store.getState());
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <React.StrictMode>
